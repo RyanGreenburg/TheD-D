1ffda0241481fa80307826453cfb4c81f2d46193
RYAN GREENBURG <ryangreenburg@mac.com>
2024-02-10 20:00:27 -0700

    Adds base project

---
+++ TheD&D/TheD_DApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheD_DApp.swift
+//  TheD&D
+//
+//  Created by RYAN GREENBURG on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TheD_DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
